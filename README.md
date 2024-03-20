e99293b873431e39cae5593631359e3908b4046d
rsmadhavan <rsmadhavan2000@gmail.com>
2024-03-20 23:04:42 +0530

    Create server and upload and file handlers

--- README.md
+++ README.md
@@ -9,3 +9,31 @@
 - Build and run
   - go build followed by ./myproject &rarr; compiles to binary
   - go run main.go  &rarr; to directly run without compiling
+
+## To check if a package is available in your workspace, you can use the go list command. Here's how:
+
+```bash
+go list -m -f {{.}} <package-import-path>
+```
+Explanation:
+
+go list: This command provides information about Go packages.
+-m: Specifies that the operation is performed on the current module and its dependencies.
+-f: Specifies the format for printing the results. In this case, we use {{.}} to print just the package path.
+Replace <package-import-path> with the import path of the package you want to check.
+
+For example, if you want to check if the package github.com/gorilla/mux is available in your workspace, you would run:
+
+bash
+Copy code
+go list -m -f {{.}} github.com/gorilla/mux
+If the package is available, this command will print the import path of the package. If it's not available, there will be no output.
+
+Make sure you are running this command in the root directory of your Go module (if you're using modules), or inside your $GOPATH/src directory (if you're not using modules).
+
+## tests
+Run tests using go test ./...
+
+
+
+
--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import ( 
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
-func main(){
-	fmt.Println("Hello Bitch")
+
+const uploadDir = "./uploads"
+
+func main() {
+    // Create uploads directory if it doesn't exist
+    if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+        os.Mkdir(uploadDir, os.ModePerm)
+    }
+
+    http.HandleFunc("/upload", uploadHandler)
+    http.HandleFunc("/files/", fileHandler)
+
+    fmt.Println("Server listening on localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "POST" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    file, handler, err := r.FormFile("file")
+    if err != nil {
+        http.Error(w, "Error retrieving file", http.StatusBadRequest)
+        return
+    }
+    defer file.Close()
+
+    // Save the file to disk
+    filepath := filepath.Join(uploadDir, handler.Filename)
+    f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        http.Error(w, "Error saving file", http.StatusInternalServerError)
+        return
+    }
+    defer f.Close()
+
+    _, err = io.Copy(f, file)
+    if err != nil {
+        http.Error(w, "Error saving file", http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Fprintf(w, "File uploaded successfully: %s", handler.Filename)
+}
+
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+    filepath := filepath.Join(uploadDir, r.URL.Path[len("/files/"):])
+    http.ServeFile(w, r, filepath)
 }
